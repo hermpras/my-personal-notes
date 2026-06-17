4358ccaee8a161148215fcdc47d07648b1c60c1f
Hermawan Prastiyanto <hermawanprastiyanto@gmail.com>
2026-06-18 00:30:30 +0700

    add datatestid note-item-delete-button

--- src/components/NoteActionButton.jsx
+++ src/components/NoteActionButton.jsx
@@ -3,6 +3,11 @@ import React from "react";
 function NoteActionButton({ variant, onClick }) {
   return (
     <button
+      data-testid={
+        variant === "delete"
+          ? "note-item-delete-button"
+          : "note-item-archive-button"
+      }
       className={
         variant === "delete"
           ? "note-item__delete-button"
